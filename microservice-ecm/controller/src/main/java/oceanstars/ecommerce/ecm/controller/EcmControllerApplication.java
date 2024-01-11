c604822dae8f485f056e3a9d6b3f0f9c2fc59bba
sgole001 <sgole001@gmail.com>
2024-01-11 13:39:39 +0800

    初始化ECM服务

---
+++ microservice-ecm/controller/src/main/java/oceanstars/ecommerce/ecm/controller/EcmControllerApplication.java
@@ -0,0 +1,22 @@
+package oceanstars.ecommerce.ecm.controller;
+
+import com.alicp.jetcache.anno.config.EnableMethodCache;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 用户对外服务启动接口
+ *
+ * @author Clover
+ * @version 1.0.0
+ * @since 2022/1/14 11:18 AM
+ */
+@EnableMethodCache(basePackages = "oceanstars.ecommerce")
+@SpringBootApplication(scanBasePackages = "oceanstars")
+public class EcmControllerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EcmControllerApplication.class, args);
+  }
+
+}
